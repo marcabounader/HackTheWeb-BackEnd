8aadc9265635a2322d524e846dd007c9a53d5152
marcabounader92 <marcabounader92@outlook.com>
2023-09-05 19:24:12 +0300

    Added score for user

--- app/Models/User.php
+++ app/Models/User.php
@@ -22,7 +22,8 @@ class User extends Authenticatable implements JWTSubject
         'name',
         'email',
         'password',
-        'type_id'
+        'type_id',
+        'score'
     ];
 
     /**
