b66e3c84d50f3f07a48047a57e8075db9a122f3f
marcabounader92 <marcabounader92@outlook.com>
2023-09-05 19:24:25 +0300

    Added score for users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -23,6 +23,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->integer('score')->nullable();
             $table->integer('rank')->nullable();
             $table->rememberToken();
             $table->timestamps();
