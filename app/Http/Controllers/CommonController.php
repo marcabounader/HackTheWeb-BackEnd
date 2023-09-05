8bf9ab0b660ca1184a24cfac561fee55e3851d42
marcabounader92 <marcabounader92@outlook.com>
2023-09-05 15:02:22 +0300

    Added get labs

--- app/Http/Controllers/CommonController.php
+++ app/Http/Controllers/CommonController.php
@@ -2,9 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Lab;
+use Exception;
 use Illuminate\Http\Request;
 
 class CommonController extends Controller
 {
-    //
+    public function getLabs(){
+        try{
+            $labs=Lab::get();
+
+            return response()->json([
+                'message' => 'Fetched labs',
+                'labs' => $labs
+            ], 200);
+        } catch(Exception $e){
+            return response()->json([
+                'message' => $e->getMessage()
+            ],500);
+        } 
+    }
 }
