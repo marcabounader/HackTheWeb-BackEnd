b6e9b25652d1e8d89e0ec3903b357cb9cea4ea2f
marcabounader92 <marcabounader92@outlook.com>
2023-09-05 14:42:38 +0300

    Added deletelab and addlab with validations

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,9 +2,63 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Lab;
+use Exception;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
-    //
+    public function addLab(Request $request){
+
+        try{
+            $lab=new Lab;
+            $lab->category_id=$request->category_id;
+            $lab->difficulty_id=$request->difficulty_id;
+            $lab->name=$request->name;
+            $lab->objective=$request->objective;
+            $lab->launch_api=$request->launch_api;
+            $lab->score=$request->score;
+            if ($lab->save()) {
+                return response()->json([
+                    'message' => 'Lab added',
+                    'lab' => $lab
+                ], 200);
+            } else {
+                return response()->json([
+                    'message' => 'Failed to add lab'
+                ], 500);
+            }
+        } catch(Exception $e){
+            return response()->json([
+                'message' => $e->getMessage()
+            ],500);
+        } 
+    }
+
+    public function deleteLab($id){
+
+        try{
+            $lab = Lab::find($id);
+
+            if (!$lab) {
+                return response()->json([
+                    'message' => 'Lab not found'
+                ], 404);
+            }
+        
+            if ($lab->delete()) {
+                return response()->json([
+                    'message' => 'Lab deleted'
+                ], 200);
+            } else {
+                return response()->json([
+                    'message' => 'Failed to delete lab'
+                ], 500);
+            }
+        } catch(Exception $e){
+            return response()->json([
+                'message' => $e->getMessage()
+            ],500);
+        } 
+    }
 }
