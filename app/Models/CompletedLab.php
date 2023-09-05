be4b2173dffe6853552c8285cdd12d04f7ca5083
marcabounader92 <marcabounader92@outlook.com>
2023-09-05 19:23:57 +0300

    Added completedlab model fillables

--- app/Models/CompletedLab.php
+++ app/Models/CompletedLab.php
@@ -8,6 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class CompletedLab extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'lab_id'
+    ];
+
     public $timestamps = false;
 
 }
