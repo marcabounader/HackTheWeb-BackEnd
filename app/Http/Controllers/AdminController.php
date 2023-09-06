4ffaf4f77883a9dab5f9ba59877b36ee363a08c1
marcabounader92 <marcabounader92@outlook.com>
2023-09-06 14:19:22 +0300

    Added modify badge category func

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -490,5 +490,39 @@ class AdminController extends Controller
         }
     }
 
+    public function modifyBadgeCategory(Request $request)
+    {
+        try {
+            $validator = Validator::make($request->all(), [
+                'id' => 'required|exists:badge_categories,id',
+                'category' => 'required|string|unique:badge_categories,category,' . $request->id,
+            ]);
+
+            if ($validator->fails()) {
+                return response()->json([
+                    'status' => 'error',
+                    'message' => $validator->errors()->first(),
+                ], 400);
+            }
+
+            $category = BadgeCategory::find($request->id);
+            $category->category = $request->category;
+
+            if ($category->save()) {
+                return response()->json([
+                    'message' => 'Badge category modified',
+                    'category' => $category
+                ], 200);
+            } else {
+                return response()->json([
+                    'message' => 'Failed to modify badge category'
+                ], 500);
+            }
+        } catch (Exception $e) {
+            return response()->json([
+                'message' => $e->getMessage()
+            ], 500);
+        }
+    }
 
 }
