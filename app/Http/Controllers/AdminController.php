f3e4552d4482569164590cbd8684e0267c1fdca5
marcabounader92 <marcabounader92@outlook.com>
2023-09-06 13:58:28 +0300

    Added add lab difficulty func

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -277,4 +277,39 @@ class AdminController extends Controller
         }
     }
 
+    public function addLabDifficulty(Request $request)
+    {
+        try {
+            $validator = Validator::make($request->all(), [
+                'name' => 'required|string|unique:lab_difficulties',
+            ]);
+
+            if ($validator->fails()) {
+                return response()->json([
+                    'status' => 'error',
+                    'message' => $validator->errors()->first(),
+                ], 400);
+            }
+
+            $difficulty = new LabDifficulty;
+            $difficulty->name = $request->name;
+
+            if ($difficulty->save()) {
+                return response()->json([
+                    'message' => 'Lab difficulty added',
+                    'difficulty' => $difficulty
+                ], 200);
+            } else {
+                return response()->json([
+                    'message' => 'Failed to add lab difficulty'
+                ], 500);
+            }
+        } catch (Exception $e) {
+            return response()->json([
+                'message' => $e->getMessage()
+            ], 500);
+        }
+    }
+
+
 }
