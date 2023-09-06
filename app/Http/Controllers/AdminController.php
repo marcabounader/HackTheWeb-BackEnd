cba272e94af449f6fb0c1db677635149da4363af
marcabounader92 <marcabounader92@outlook.com>
2023-09-06 13:53:22 +0300

    Added addlabcategory func

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -8,6 +8,7 @@ use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Validator;
 
 class AdminController extends Controller
 {
@@ -170,5 +171,41 @@ class AdminController extends Controller
             ], 500);
         }
     }
-    
+
+    public function addLabCategory(Request $request)
+    {
+        try {
+            $validator = Validator::make($request->all(), [
+                'name' => 'required|string|unique:lab_categories',
+            ]);
+
+            if ($validator->fails()) {
+                return response()->json([
+                    'status' => 'error',
+                    'message' => $validator->errors()->first(),
+                ], 400);
+            }
+
+            $category = new LabCategory;
+            $category->name = $request->name;
+
+            if ($category->save()) {
+                return response()->json([
+                    'message' => 'Lab category added',
+                    'category' => $category
+                ], 200);
+            } else {
+                return response()->json([
+                    'message' => 'Failed to add lab category'
+                ], 500);
+            }
+        } catch (Exception $e) {
+            return response()->json([
+                'message' => $e->getMessage()
+            ], 500);
+        }
+    }
+
+ 
+
 }
