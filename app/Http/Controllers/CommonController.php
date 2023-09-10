259d62843797775e61da2d55eb7ee864e778dc83
marcabounader92 <marcabounader92@outlook.com>
2023-09-10 17:37:23 +0300

    Added isactive and iscomplete

--- app/Http/Controllers/CommonController.php
+++ app/Http/Controllers/CommonController.php
@@ -2,17 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\ActiveLab;
 use App\Models\Badge;
+use App\Models\CompletedLab;
 use App\Models\Lab;
 use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class CommonController extends Controller
 {
     public function getLabs(){
         try{
             $labs=Lab::with('difficultyInfo')->get();
-
+            foreach ($labs as $lab) {
+                $lab->isActive = ActiveLab::where([['user_id','=',Auth::id()],['lab_id','=',$lab->id]])
+                    ->exists();
+                $lab->isComplete = CompletedLab::where([['user_id','=',Auth::id()],['lab_id','=',$lab->id]])
+                ->exists();
+            }
             return response()->json([
                 'message' => 'Fetched labs',
                 'labs' => $labs
