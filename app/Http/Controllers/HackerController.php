deb7ba856f813f53536a0c29930bc24c7b1fb42f
marcabounader92 <marcabounader92@outlook.com>
2023-09-12 14:38:40 +0300

    Removed ldap for sqli lab

--- app/Http/Controllers/HackerController.php
+++ app/Http/Controllers/HackerController.php
@@ -62,7 +62,6 @@ class HackerController extends Controller
                 dockerfile: Dockerfile
             networks:
               - datanet   
-            stop_grace_period: 1m
         
           database_admin:
             container_name: database_admin-$user_id
@@ -76,42 +75,11 @@ class HackerController extends Controller
               - 127.0.0.1::80
             networks:
               - datanet   
-            stop_grace_period: 1m
-        
-          directory:
-            container_name: directory-$user_id
-            image: webpwnized/mutillidae:ldap
-            build:
-                context: ./ldap
-                dockerfile: Dockerfile
-            volumes:
-              - ldap_data:/var/lib/ldap
-              - ldap_config:/etc/ldap/slapd.d
-            ports:
-              - 127.0.0.1::389
-            networks:
-              - ldapnet
-            stop_grace_period: 1h
-        
-          directory_admin:
-            container_name: directory_admin-$user_id
-            depends_on:
-              - directory
-            image: webpwnized/mutillidae:ldap_admin
-            build:
-                context: ./ldap_admin          
-                dockerfile: Dockerfile
-            ports:
-              - 127.0.0.1::80
-            networks:
-              - ldapnet
-            stop_grace_period: 1h
               
           www-sqli:
               container_name: www-sqli-$user_id
               depends_on:
                 - database
-                - directory
               image: webpwnized/mutillidae:www-sqli
               build:
                   context: ../../../../storage/mutillidae-docker-master/www-sqli
@@ -121,10 +89,8 @@ class HackerController extends Controller
                 - 127.0.0.1::443
               networks:
                 - datanet
-                - ldapnet
               environment:
               - FLAG= flag-{$randomFlag}
-              stop_grace_period: 1h
         # Volumes to persist data used by the LDAP server
         volumes:
           ldap_data:
@@ -169,7 +135,7 @@ class HackerController extends Controller
             return response()->json([
                 'message' => "Error starting instance for user ID {$user_id}",
                 'output' => $output, // Capture the command output
-            ]);
+            ],500);
         }
     }
     
