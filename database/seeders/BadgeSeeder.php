ca7d4ee79eeb69de9717207d2a6b034d893ac5fc
marcabounader92 <marcabounader92@outlook.com>
2023-09-19 16:41:33 +0300

    Added badge seeder

---
+++ database/seeders/BadgeSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class BadgeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $labs = DB::table('labs')->whereNot('id','1')->get();
+
+        foreach ($labs as $lab) {
+            $categoryId = $lab->id % 3 + 1;
+
+            DB::table('badges')->insert([
+                'category_id' => $categoryId, 
+                'name' => 'Badge'.$lab->id,
+                'icon_url' => 'http://localhost:8000/storage/badges/medal-solid.svg',
+                'lab_id' => $lab->id
+            ]);
+        }
+    }
+}
